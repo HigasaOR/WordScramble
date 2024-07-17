b1fa5d671b6c876da12522ed3570f99aa806b880
HigasaOR <lee1003094395@gmail.com>
2024-07-17 22:37:27 +0800

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Chien Lee on 2024/7/17.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
